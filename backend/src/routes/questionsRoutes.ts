f88d3133e0e6951858d97b39bffe9dfe8621414c
Nuno <nunomarquees@gmail.com>
2026-03-24 09:57:11 +0000

    feat: add questions routes

---
+++ backend/src/routes/questionsRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { getQuestions, getQuestionById, createQuestion, updateQuestion, deleteQuestion } from '../controllers/questionsControllers';
+
+const router = Router();
+
+router.get("/", getQuestions);
+router.get("/:id", getQuestionById);
+router.post("/", createQuestion);
+router.put("/:id", updateQuestion);
+router.delete("/:id", deleteQuestion);
+
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,6 @@
-import express from "express";
-import cors from "cors";
+import app from "./app"
 
-const app = express();
 const PORT = 3000;
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("API is running!");
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on door ${PORT}`);
