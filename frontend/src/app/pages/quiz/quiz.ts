bae21ad524296da336aa8b5a231984d307beb193
Nuno <nunomarquees@gmail.com>
2026-03-31 15:13:32 +0100

    feat: quiz html and ts

--- frontend/src/app/pages/quiz/quiz.ts
+++ frontend/src/app/pages/quiz/quiz.ts
@@ -1,9 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { ApiService } from '../../services/api';
+import { Question } from '../../models/question.model';
 
 @Component({
   selector: 'app-quiz',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './quiz.html',
-  styleUrl: './quiz.css',
+  styleUrl: './quiz.css'
 })
-export class Quiz {}
+export class Quiz implements OnInit {
+  private api = inject(ApiService);
+  private router = inject(Router);
+
+  questions: Question[] = [];
+  currentIndex = 0;
+  selectedAnswer = '';
+  score = 0;
+  finished = false;
+  showExplanation = false;
+  answered = false;
+
+  ngOnInit(): void {
+    this.api.getQuiz().subscribe({
+      next: (data) => this.questions = data,
+      error: () => this.router.navigate(['/'])
+    });
+  }
+
+  get currentQuestion(): Question {
+    return this.questions[this.currentIndex];
+  }
+
+  selectAnswer(option: string): void {
+    if (this.answered) return;
+    this.selectedAnswer = option;
+    this.answered = true;
+    this.showExplanation = true;
+    if (option === this.currentQuestion.correct_answer) {
+      this.score++;
+    }
+  }
+
+  next(): void {
+    if (this.currentIndex < this.questions.length - 1) {
+      this.currentIndex++;
+      this.selectedAnswer = '';
+      this.answered = false;
+      this.showExplanation = false;
+    } else {
+      this.finished = true;
+      this.saveResult();
+    }
+  }
+
+  saveResult(): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.api.saveResult(this.score, this.questions.length).subscribe();
+    }
+  }
+
+  restart(): void {
+    this.router.navigate(['/quiz']);
+  }
+
+  goToHistory(): void {
+  this.router.navigate(['/history']);
+  }
+}
