0ba0d5b0ef8a483afded7eae4a4a1fb6e69d2d76
Nuno <nunomarquees@gmail.com>
2026-04-07 00:27:16 +0100

    fix: vercel url 2.0

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:4200',
-        'https://mental-health-quiz-nunazo987s.vercel.app/'
+        'https://mental-health-quiz-nunazo987s.vercel.app'
     ]
 }));
 app.use(express.json());
